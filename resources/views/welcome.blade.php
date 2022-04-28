112ff482ae80667cb27d2f1979adeef149fb786c
Marcelo Almeida <msahardware@hotmail.com>
2022-04-27 23:31:42 -0300

    First commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel11
+                    Laravel1111
                 </div>
 
                 <div class="links">
