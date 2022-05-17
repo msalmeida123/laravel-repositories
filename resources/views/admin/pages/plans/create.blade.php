52f2c88b2531c7ec88c25629f8ccc0354e75e41b
Marcelo Almeida <msahardware@hotmail.com>
2022-05-17 18:17:35 -0300

    commit

--- resources/views/admin/pages/plans/create.blade.php
+++ resources/views/admin/pages/plans/create.blade.php
@@ -10,29 +10,29 @@
 
 
    <div class='card'>
-      <div class='card-body'>
-        <form action="{{ route('plans.store') }}" class="form" method="POST">
-         @csrf
+        <div class='card-body'>
+            <form action="{{ route('plans.store') }}" class="form" method="POST">
+              @csrf
 
-        <div class="form-group">
-            <label>Nome:</label>
-            <input type="text" name="name" class="form-control" placeholder="Nome">
-        </div>
-        <div class="form-group">
-            <label>Preço:</label>
-            <input type="text" name="price" class="form-control" placeholder="Preço">
-        </div>
-        <div class="form-group">
-            <label>Descrição:</label>
-            <input type="text" name="description" class="form-control" placeholder="Descrição">
-        </div>
-        <div class="form-group">
-            <button type="submit" class=" btn btn-dark">Enviar</button>
-        </div>
+            <div class="form-group">
+                 <label>Nome:</label>
+                 <input type="text" name="name" class="form-control" placeholder="Nome">
+            </div>
+            <div class="form-group">
+                 <label>Preço:</label>
+                  <input type="text" name="price" class="form-control" placeholder="Preço">
+             </div>
+             <div class="form-group">
+                   <label>Descrição:</label>
+                   <input type="text" name="description" class="form-control" placeholder="Descrição">
+             </div>
+             <div class="form-group">
+                  <button type="submit" class=" btn btn-dark">Enviar</button>
+             </div>
 
 
 
-     </div>
+        </div>
 
     </div>
 @endsection
